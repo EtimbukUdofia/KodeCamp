700371eaa4315b55fbd3663b388d8b1cbdc8a60d
EtimbukUdofia <etimbukudofia9@gmail.com>
2025-06-15 00:20:59 +0100

    feat: add history feature This tracks all operations performed on the counter

--- Stage1/counter_factory/index.js
+++ Stage1/counter_factory/index.js
@@ -1,14 +1,28 @@
 const CounterPrototype = {
   increment() {
     this._setCount(this._getCount() + 1);
+    this._setHistory({
+      operation: "Increment",
+      value: this.getValue(),
+      time: new Date().toString(),
+    });
   },
 
   decrement() {
     this._setCount(this._getCount() - 1);
+    this._setHistory({
+      operation: "Decrement",
+      value: this.getValue(),
+      time: new Date().toString(),
+    });
   },
 
   getValue() {
     return this._getCount();
+  },
+
+  getHistory() {
+    return this._getHistory();
   },
 
   reset() {
@@ -18,6 +32,11 @@ const CounterPrototype = {
   transform(transformFn) {
     this._setCount(transformFn(this._getCount()));
     // return this.getValue();
+    this._setHistory({
+      operation: "Transform",
+      value: this.getValue(),
+      time: new Date().toString(),
+    });
     return this._getCount();
   },
 
@@ -66,14 +85,19 @@ const CounterPrototype = {
     console.log(increments, decrements);
   },
 
+  toString() {
+    return `This is a counter factory and the current value is ${this.getValue()}`;
+  },
+
   getConfig() {
     return this._config;
-  }
+  },
 };
 
 const createCounter = (initialValue = 0) => {
   let count = initialValue;
   let originalValue = initialValue;
+  let history = [];
 
   const counter = Object.create(CounterPrototype);
 
@@ -82,11 +106,18 @@ const createCounter = (initialValue = 0) => {
     count = value;
   };
   counter._getInitialValue = () => originalValue;
+  counter._getHistory = () => history;
+  counter._setHistory = (value) => history.push(value);
 
   return counter;
 };
 
-const createAdvancedCounter = ({ initialValue = 0, step = 1, min = -Infinity, max = Infinity } = {}) => {
+const createAdvancedCounter = ({
+  initialValue = 0,
+  step = 1,
+  min = -Infinity,
+  max = Infinity,
+} = {}) => {
   let count = initialValue;
   originalValue = initialValue;
 
@@ -113,26 +144,28 @@ counter2.decrement();
 console.log(counter1.getValue()); // 4
 console.log(counter2.getValue()); // 4
 
-console.log(counter1.transform(x => x * 2)); // 8
+console.log(counter1.transform((x) => x * 2)); // 8
 
-console.log(counter1.transform(x => x - 3)); // 5
+console.log(counter1.transform((x) => x - 3)); // 5
 
-counter1.onChange((value, operation) => {
-  console.log(`New Value is ${value} and the operation is ${operation}`)
-}).increment().decrement();
+counter1
+  .onChange((value, operation) => {
+    console.log(`New Value is ${value} and the operation is ${operation}`);
+  })
+  .increment();
 
 const baseCounter = createCounter(10);
 
-const addCounter = baseCounter.add(5);        // 15
+const addCounter = baseCounter.add(5); // 15
 const subtractCounter = baseCounter.subtract(3); // 7
 const multiplyCounter = baseCounter.multiply(2); // 20
 
-console.log(baseCounter.getValue());  // 10
-console.log(addCounter.getValue());        // 15
-console.log(subtractCounter.getValue());   // 7
-console.log(multiplyCounter.getValue());   // 20
+console.log(baseCounter.getValue()); // 10
+console.log(addCounter.getValue()); // 15
+console.log(subtractCounter.getValue()); // 7
+console.log(multiplyCounter.getValue()); // 20
 
-
+console.log(counter1.getHistory());
 
 // counter1.decrement();
 // console.log(counter1.getValue());
@@ -145,7 +178,6 @@ console.log(multiplyCounter.getValue());   // 20
 // const counter3 = counter1.add(5);
 // console.log(counter3.getValue());
 // counter3.batch({ increments: 1, decrements: 3, notUsed: 4 });
-
 
 // const advancedCounter = createAdvancedCounter();
 // console.log(advancedCounter.getConfig().max);
